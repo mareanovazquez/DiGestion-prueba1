ec5b83f449144738636752f6376d00cdc6629564
Matias Aranda <sergiomatiasaranda@gmail.com>
2023-05-10 13:39:40 -0300

    ajuste

--- src/components/AddRemito/AddRemito.jsx
+++ src/components/AddRemito/AddRemito.jsx
@@ -73,7 +73,7 @@ export const AddRemito = () => {
                 <div className="row altaRemito">
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         {/* <button class="btn btn-primary me-md-2" type="button">Button</button> */}
-                        <button class="btn btn-primary" type="button">Button</button>
+                        <button class="btn btn-primary" type="button">Guardar</button>
                     </div>
 
 
