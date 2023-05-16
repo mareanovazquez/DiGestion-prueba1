b2a73b6f887b6f576f678b43f6f6b66e3d807d90
Matias Aranda <sergiomatiasaranda@gmail.com>
2023-05-16 10:24:34 -0300

    Integracion con azure B2C

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,197 @@
-import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import { useEffect } from 'react';
+import { MsalProvider, useMsal } from '@azure/msal-react';
+import { EventType } from '@azure/msal-browser';
+
+import { PageLayout } from './components/PageLayout';
+
+import { Home } from './pages/Home';
+
+import { b2cPolicies, protectedResources } from './authConfig';
+import { compareIssuingPolicy } from './utils/claimUtils';
+
+ import { ListProveedores } from './components/ListProveedores/ListProveedores'
+ import { ListaRemitos } from './components/ListaRemitos/ListaRemitos.jsx';
+ import { NavBar } from './components/NavBar/NavBar';
+ import { FooterNav } from './components/FooterNav/FooterNav';
+ import { BrowserRouter, Route, Switch } from 'react-router-dom';
+ import { ItemListRemito } from './components/ItemListRemito/ItemListRemito.jsx';
+ import { ListaRemitos2 } from './components/ListaRemitos2/ListaRemitos2';
+ import { ItemRemito } from './components/ItemRemito/ItemRemito';
+ import { AddRemito } from './components/AddRemito/AddRemito';
 
 
-import { ListProveedores } from './components/ListProveedores/ListProveedores'
-import { ListaRemitos } from './components/ListaRemitos/ListaRemitos.jsx';
-import { NavBar } from './components/NavBar/NavBar';
-import { FooterNav } from './components/FooterNav/FooterNav';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { LogIn } from './components/LogIn/LogIn';
-import { ItemListRemito } from './components/ItemListRemito/ItemListRemito.jsx';
-import { ListaRemitos2 } from './components/ListaRemitos2/ListaRemitos2';
-import { ItemRemito } from './components/ItemRemito/ItemRemito';
-import { AddRemito } from './components/AddRemito/AddRemito';
+import './App.css';
 
+const Pages = () => {
+    /**
+     * useMsal is hook that returns the PublicClientApplication instance,
+     * an array of all accounts currently signed in and an inProgress value
+     * that tells you what msal is currently doing. For more, visit:
+     * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
+     */
+    const { instance } = useMsal();
+    useEffect(() => {
+        const callbackId = instance.addEventCallback((event) => {
+            if (
+                (event.eventType === EventType.LOGIN_SUCCESS || event.eventType === EventType.ACQUIRE_TOKEN_SUCCESS) &&
+                event.payload.account
+            ) {
+                /**
+                 * For the purpose of setting an active account for UI update, we want to consider only the auth
+                 * response resulting from SUSI flow. "tfp" claim in the id token tells us the policy (NOTE: legacy
+                 * policies may use "acr" instead of "tfp"). To learn more about B2C tokens, visit:
+                 * https://docs.microsoft.com/en-us/azure/active-directory-b2c/tokens-overview
+                 */
+                if (compareIssuingPolicy(event.payload.idTokenClaims, b2cPolicies.names.editProfile)) {
+                    // retrieve the account from initial sing-in to the app
+                    const originalSignInAccount = instance
+                        .getAllAccounts()
+                        .find(
+                            (account) =>
+                                account.idTokenClaims.oid === event.payload.idTokenClaims.oid &&
+                                account.idTokenClaims.sub === event.payload.idTokenClaims.sub && 
+                                compareIssuingPolicy(account.idTokenClaims, b2cPolicies.names.signUpSignIn)        
+                        );
 
+                    let signUpSignInFlowRequest = {
+                        authority: b2cPolicies.authorities.signUpSignIn.authority,
+                        account: originalSignInAccount,
+                    };
 
+                    // silently login again with the signUpSignIn policy
+                    instance.ssoSilent(signUpSignInFlowRequest);
+                }
 
+                /**
+                 * Below we are checking if the user is returning from the reset password flow.
+                 * If so, we will ask the user to reauthenticate with their new password.
+                 * If you do not want this behavior and prefer your users to stay signed in instead,
+                 * you can replace the code below with the same pattern used for handling the return from
+                 * profile edit flow
+                 */
+                if (compareIssuingPolicy(event.payload.idTokenClaims, b2cPolicies.names.forgotPassword)) {
+                    let signUpSignInFlowRequest = {
+                        authority: b2cPolicies.authorities.signUpSignIn.authority,
+                        scopes: [
+                            ...protectedResources.apiTodoList.scopes.read,
+                            ...protectedResources.apiTodoList.scopes.write,
+                        ],
+                    };
+                    instance.loginRedirect(signUpSignInFlowRequest);
+                }
+            }
 
+            if (event.eventType === EventType.LOGIN_FAILURE) {
+                // Check for forgot password error
+                // Learn more about AAD error codes at https://docs.microsoft.com/en-us/azure/active-directory/develop/reference-aadsts-error-codes
+                if (event.error && event.error.errorMessage.includes('AADB2C90118')) {
+                    const resetPasswordRequest = {
+                        authority: b2cPolicies.authorities.forgotPassword.authority,
+                        scopes: [],
+                    };
+                    instance.loginRedirect(resetPasswordRequest);
+                }
+            }
+        });
 
-function App() {
+        return () => {
+            if (callbackId) {
+                instance.removeEventCallback(callbackId);
+            }
+        };
+        // eslint-disable-next-line
+    }, [instance]);
 
-
-  return (
-    <>
-    <BrowserRouter>
-
+    return (
+      <BrowserRouter>
       <NavBar />
-
-      <Routes>
-        <Route path='/' element={<LogIn /> }/>
+        <Switch>
+           
+            <Route path="/" element={<Home />} />
+          
         
 
-        {/* <AddRemito /> */}
-        <Route path='/proveedores' element={<ListProveedores />}/>
-        <Route path='/remitos' element={<ItemListRemito />} />
-        <Route path='/remito/:pid' element={<ItemRemito />} />
+                 {/* <AddRemito /> */}
+                 <Route path='/proveedores' element={<ListProveedores />}/>
+                 <Route path='/remitos' element={<ItemListRemito />} />
+                 <Route path='/remito/:pid' element={<ItemRemito />} />
+               
        
-      </Routes>
-      {/* <FooterNav /> */}
+              {/* <FooterNav /> */}
+        
+             
+        </Switch>
+        </BrowserRouter>
+    );
+};
 
-    </BrowserRouter>
-  </>
-  )
-}
+/**
+ * msal-react is built on the React context API and all parts of your app that require authentication must be
+ * wrapped in the MsalProvider component. You will first need to initialize an instance of PublicClientApplication
+ * then pass this to MsalProvider as a prop. All components underneath MsalProvider will have access to the
+ * PublicClientApplication instance via context as well as all hooks and components provided by msal-react. For more, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+ */
+const App = ({ instance }) => {
+    return (
+        <MsalProvider instance={instance}>
+            <PageLayout>
+                <Pages />
+            </PageLayout>
+        </MsalProvider>
+    );
+};
+
+export default App;
 
 
-export default App
+
+// import viteLogo from '/vite.svg'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css'
+
+
+// import { ListProveedores } from './components/ListProveedores/ListProveedores'
+// import { ListaRemitos } from './components/ListaRemitos/ListaRemitos.jsx';
+// import { NavBar } from './components/NavBar/NavBar';
+// import { FooterNav } from './components/FooterNav/FooterNav';
+// import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+// import { LogIn } from './components/LogIn/LogIn';
+// import { ItemListRemito } from './components/ItemListRemito/ItemListRemito.jsx';
+// import { ListaRemitos2 } from './components/ListaRemitos2/ListaRemitos2';
+// import { ItemRemito } from './components/ItemRemito/ItemRemito';
+// import { AddRemito } from './components/AddRemito/AddRemito';
+
+
+
+
+
+
+// function App() {
+
+
+//   return (
+//     <>
+//     <BrowserRouter>
+
+//       <NavBar />
+
+//       <Routes>
+//         <Route path='/' element={<LogIn /> }/>
+        
+
+//         {/* <AddRemito /> */}
+//         <Route path='/proveedores' element={<ListProveedores />}/>
+//         <Route path='/remitos' element={<ItemListRemito />} />
+//         <Route path='/remito/:pid' element={<ItemRemito />} />
+       
+//       </Routes>
+//       {/* <FooterNav /> */}
+
+//     </BrowserRouter>
+//   </>
+//   )
+// }
+
+
+// export default App
