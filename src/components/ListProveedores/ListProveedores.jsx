6a41a80f1668cb05d8dc9283d5963338d63967fb
Matias Aranda <sergiomatiasaranda@gmail.com>
2023-05-19 12:32:05 -0300

    agrego codigo para que si el usuario no esta autenticado no pueda ver listado de proveedores

--- src/components/ListProveedores/ListProveedores.jsx
+++ src/components/ListProveedores/ListProveedores.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { Filter } from "../Filter/Filters";
 import { Link } from "react-router-dom";
 import descargaProveedor from '../../assets/download.svg'
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 export const ListProveedores = () => {
 
     const [proveedores, setProveedores] = useState([]);
-    
+
 
     useEffect(() => {
         const FetchProveedores = async () => {
@@ -14,7 +15,7 @@ export const ListProveedores = () => {
             const proveedores = result.data
 
             setProveedores(proveedores);
-            
+
         }
 
         FetchProveedores()
@@ -25,87 +26,91 @@ export const ListProveedores = () => {
 
 
         return (
-            <div className="contenedorRemitos">
-                <hr></hr>
-                <h2 className="text-center"> Lista de Proveedores</h2>
+            <><AuthenticatedTemplate>
+                <div className="contenedorRemitos">
+                    <hr></hr>
+                    <h2 className="text-center"> Lista de Proveedores</h2>
 
 
 
-                <br></br>
-                <input type='text' value={filterState} placeholder="Filtrar" onChange={handleFilterChange} />
-                <table className="table tablaRemitos">
-                    <thead>
-                        <tr>
-                            <th className="text-center" scope='col'> ID</th>
-                            <th scope='col'> Nombre</th>
-                            <th scope='col'> Estado</th>
-                            <th scope='col'> Creado por</th>
-                            <th scope='col'> Actualización</th>
-                            <th scope='col'> Fecha creación</th>
-                            <th scope="col">Fecha actualización</th>
-                            <th scope="col">Descarga</th>
-                        </tr>
-                    </thead>
+                    <br></br>
+                    <input type='text' value={filterState} placeholder="Filtrar" onChange={handleFilterChange} />
+                    <table className="table tablaRemitos">
+                        <thead>
+                            <tr>
+                                <th className="text-center" scope='col'> ID</th>
+                                <th scope='col'> Nombre</th>
+                                <th scope='col'> Estado</th>
+                                <th scope='col'> Creado por</th>
+                                <th scope='col'> Actualización</th>
+                                <th scope='col'> Fecha creación</th>
+                                <th scope="col">Fecha actualización</th>
+                                <th scope="col">Descarga</th>
+                            </tr>
+                        </thead>
 
-                    <tbody>
-                        {filterState === ''
+                        <tbody>
+                            {filterState === ''
 
-                            ? proveedores.map((proveedor) =>
-                            (
-
-
-                                <tr className="border-bottom my-2" key={proveedor.id} scope="row">
-                                    <td className="border-bottom">{proveedor.nombre}</td>
-                                    <td className="border-bottom">{proveedor.estado}</td>
-                                    <td className="border-bottom">{proveedor.created_by}</td>
-                                    <td className="border-bottom">{proveedor.updated_by}</td>
-                                    <td className="border-bottom">{proveedor.created_at}</td>
-                                    <td className="border-bottom">{proveedor.updated_at}</td>
-                                    <td className="border-bottom"><Link to={`/proveedor/${proveedor.id}`}><img src={descargaProveedor} alt="" /></Link></td>
-                                </tr>
-
-
-
-                            )
-
-                            )
-                            : proveedores.filter(proveedor => proveedor.nombre.toString().toLowerCase().includes(filterState.toLowerCase()) ||
-                                proveedor.estado.toString().toLowerCase().includes(filterState.toLowerCase()))
-                                .map((proveedor) => (
+                                ? proveedores.map((proveedor) => (
 
 
                                     <tr className="border-bottom my-2" key={proveedor.id} scope="row">
                                         <td className="border-bottom">{proveedor.nombre}</td>
-                                    <td className="border-bottom">{proveedor.estado}</td>
-                                    <td className="border-bottom">{proveedor.created_by}</td>
-                                    <td className="border-bottom">{proveedor.updated_by}</td>
-                                    <td className="border-bottom">{proveedor.created_at}</td>
-                                    <td className="border-bottom">{proveedor.updated_at}</td>
-                                    <td className="border-bottom"><Link to={`/proveedor/${proveedor.id}`}><img src={descargaProveedor} alt="" /></Link></td>
+                                        <td className="border-bottom">{proveedor.estado}</td>
+                                        <td className="border-bottom">{proveedor.created_by}</td>
+                                        <td className="border-bottom">{proveedor.updated_by}</td>
+                                        <td className="border-bottom">{proveedor.created_at}</td>
+                                        <td className="border-bottom">{proveedor.updated_at}</td>
+                                        <td className="border-bottom"><Link to={`/proveedor/${proveedor.id}`}><img src={descargaProveedor} alt="" /></Link></td>
                                     </tr>
 
 
-                                )
-
-
 
                                 )
-                        }
-                    </tbody>
-                </table>
 
-            </div >
+                                )
+                                : proveedores.filter(proveedor => proveedor.nombre.toString().toLowerCase().includes(filterState.toLowerCase()) ||
+                                    proveedor.estado.toString().toLowerCase().includes(filterState.toLowerCase()))
+                                    .map((proveedor) => (
+
+
+                                        <tr className="border-bottom my-2" key={proveedor.id} scope="row">
+                                            <td className="border-bottom">{proveedor.nombre}</td>
+                                            <td className="border-bottom">{proveedor.estado}</td>
+                                            <td className="border-bottom">{proveedor.created_by}</td>
+                                            <td className="border-bottom">{proveedor.updated_by}</td>
+                                            <td className="border-bottom">{proveedor.created_at}</td>
+                                            <td className="border-bottom">{proveedor.updated_at}</td>
+                                            <td className="border-bottom"><Link to={`/proveedor/${proveedor.id}`}><img src={descargaProveedor} alt="" /></Link></td>
+                                        </tr>
+
+
+                                    )
+
+
+
+                                    )}
+                        </tbody>
+                    </table>
+
+                </div>
+            </AuthenticatedTemplate>
+                <UnauthenticatedTemplate>
+                    <h2 className="text-center"> NO ESTAS AUTENTICADO LOCO</h2>
+
+                </UnauthenticatedTemplate></>
         )
-    }   
-    
+    }
+
 
 
     return (
 
         <>
-           <Filter>
-            {handleProductFiltered}
-           </Filter>
+            <Filter>
+                {handleProductFiltered}
+            </Filter>
         </>
     )
 }
