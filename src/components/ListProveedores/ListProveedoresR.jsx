ff261964f4fb8b944f51b8a10f22c5e696d09345
Mariano Dario Vazquez <mdvazquez@do.scba.gov.ar>
2023-06-13 13:53:05 -0300

    Aplicación de loading a los componentes de proveedores

--- src/components/ListProveedores/ListProveedoresR.jsx
+++ src/components/ListProveedores/ListProveedoresR.jsx
@@ -122,7 +122,7 @@ export const ListProveedoresR = () => {
                         {handleProductFiltered}
                     </Filter>
 
-                 
+        
             }
 
         </>
