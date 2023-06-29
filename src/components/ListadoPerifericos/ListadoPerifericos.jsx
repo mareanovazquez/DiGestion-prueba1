fb9ef6df032ddef0432c83c6ee1cf3c721782c06
mareanovazquez <mareanovazquez@protonmail.com>
2023-06-29 15:34:20 -0300

    corrección en la funcionalidad de los botones de listado perifericos

--- src/components/ListadoPerifericos/ListadoPerifericos.jsx
+++ src/components/ListadoPerifericos/ListadoPerifericos.jsx
@@ -208,7 +208,6 @@ export const ListadoPerifericos = () => {
     //Función para eliminar un solo item de la lista de perifericos 
     const DeleteItem = (id)=> {
 
-        
         setPerifericoSeleccionado('');
         setMarcaSeleccionada('');
         setModeloSeleccionado('');
@@ -454,11 +453,12 @@ export const ListadoPerifericos = () => {
                                     </table>
                                     <div>
                                         <p>Total de periféricos cargados: <b>{totalCantidad}</b></p>
-                                        <Button variant="primary" >Enviar</Button>
-                                        <Button variant="secondary" onClick={EliminarTodo}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash trashIcon" viewBox="0 0 16 16">
+                                        <div className="d-inline"><Button variant="primary" >Enviar</Button></div> 
+                                        <div className="d-inline ps-3"><Button variant="secondary" onClick={EliminarTodo}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash trashIcon" viewBox="0 0 16 16">
                                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                                        </svg></Button>
+                                        </svg>
+                                        </Button></div>
                                     </div>
                                 </div>
                             )}
