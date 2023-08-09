7445fb9c4fbe21cc891c6d84a535da4f49ea0629
Mariano Vazquez <mareanovazquez@protonmail.com>
2023-08-09 14:40:34 -0300

    rendering codicional del LogIn y de la pantalla inicio

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -3,20 +3,25 @@ import { UserContext } from '../../UserContext/UserContext';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Inicio } from '../Inicio/Inicio';
 
 
 
 export const LogIn = () => {
 
 
-    const {  handleLogin, username,setUsername, email, setEmail, password, setPassword, error, usuarios, setUsuario, permisos, setPermisos, handleButtonLogIn } = useContext(UserContext);
-    
+    const { handleLogin, username, setUsername, email, setEmail, password, setPassword, error, usuarios, setUsuario, permisos, setPermisos, loggedIn, setLoggedIn, handleButtonLogIn } = useContext(UserContext);
+
 
     return (
-        <div className='contenedorLogIn'>
-            <Form className='formLogIn' onSubmit={handleLogin}>
-                <h2 className='text-center'>DIGESTIÓN</h2>
-              {/*   <Form.Group className="mb-3" controlId="formBasicUsername">
+        <>
+            {loggedIn ?
+                <Inicio />
+                :
+                <div className='contenedorLogIn'>
+                    <Form className='formLogIn' onSubmit={handleLogin}>
+                        <h2 className='text-center'>DIGESTIÓN</h2>
+                        {/*   <Form.Group className="mb-3" controlId="formBasicUsername">
                     <Form.Label>Usuario</Form.Label>
                     <Form.Control type="text" placeholder="USUARIO" value={username} onChange={(e) => setUsername(e.target.value)} />
                     <Form.Text className="text-muted">
@@ -24,25 +29,27 @@ export const LogIn = () => {
                     </Form.Text>
                 </Form.Group> */}
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="EMAIL" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <Form.Text className="text-muted">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="EMAIL" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Text className="text-muted">
 
-                    </Form.Text>
-                </Form.Group>
+                            </Form.Text>
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Constraseña</Form.Label>
-                    <Form.Control type="password" placeholder="CONTRASEÑA" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Constraseña</Form.Label>
+                            <Form.Control type="password" placeholder="CONTRASEÑA" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </Form.Group>
 
-                <Button onClick={handleButtonLogIn} variant="primary" type="submit">
-                    Ingresar
-                </Button>
-                {error === 'Request failed with status code 500' ? <p>Invalid username or password </p>: <p>{error}</p>}
-            </Form>
-        </div>
+                        <Button onClick={handleButtonLogIn} variant="primary" type="submit">
+                            Ingresar
+                        </Button>
+                        {error === 'Request failed with status code 500' ? <p>Invalid username or password </p> : <p>{error}</p>}
+                    </Form>
+                </div>
+            }
+        </>
     );
 
 
