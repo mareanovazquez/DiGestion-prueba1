3d39f713b8759c11a3a3b4de0ee862c04ef8aee9
Mariano Vazquez <mareanovazquez@protonmail.com>
2023-09-01 13:11:22 -0300

    borrado manual de forms de AddRemito

--- src/components/AddRemito/AddRemito.jsx
+++ src/components/AddRemito/AddRemito.jsx
@@ -17,6 +17,12 @@ export const AddRemito = () => {
     const [selectedValueProv, setSelectedValueProv] = useState({ label: 'Proveedores', value: '' });
     const [selectedValueDep, setSelectedValueDep] = useState({ label: 'Departamentos', value: '' });
 
+    const deleteCamposRemito = (resetFormFunction) => {
+        resetFormFunction();
+        // Aquí también puedes resetear otros estados si lo necesitas, por ejemplo:
+        setSelectedValueProv({ label: 'Proveedores', value: '' });
+        setSelectedValueDep({ label: 'Departamentos', value: '' });
+    }
 
 
     return (
@@ -25,8 +31,8 @@ export const AddRemito = () => {
                 <Formik
                     initialValues={{
                         remito: '',
-                        departamento:'',
-                        proveedor:'',
+                        departamento: '',
+                        proveedor: '',
                         fechaRecepcionSTI: '',
                         expediente: '',
                         legajoCompra: '',
@@ -60,23 +66,13 @@ export const AddRemito = () => {
                         resetForm();
                         setShow(true)
                     }}
+
                 >
-                    {({errors, setFieldValue, values}) => (
+                    {({ errors, setFieldValue, values, resetForm }) => (
 
                         <Form>
                             <div className="row altaRemito">
                                 <h2 className="text-left"> Alta de Remito</h2>
-                                {/*Nombre del usuario que da el alta del remito */}
-                                {/* <div className="col">
-                                    <label>Usuario Alta</label>
-                                    <Field type="text"
-                                        className="form-control"
-                                        placeholder="Usuario"
-                                        aria-label="Usuario Ata"
-                                        value={name}
-                                        disabled
-                                    />
-                                </div> */}
 
                                 <div className="col">
                                     <label>Remito</label>
@@ -99,9 +95,9 @@ export const AddRemito = () => {
                                         selectedValueDep={values.departamento}
                                         onChange={(selectedDepartamento) => {
                                             setFieldValue('departamento', selectedDepartamento);
-                                            setSelectedValueDep(selectedDepartamento); 
+                                            setSelectedValueDep(selectedDepartamento);
                                         }} />
-                                         <ErrorMessage
+                                    <ErrorMessage
                                         name="departamento"
                                         component={() => (<span><small className="text-danger p-2">{errors.departamento}</small></span>)}
                                     />
@@ -113,9 +109,9 @@ export const AddRemito = () => {
                                         selectedValueProv={values.proveedor}
                                         onChange={(selectedProveedor) => {
                                             setFieldValue('proveedor', selectedProveedor);
-                                            setSelectedValueProv(selectedProveedor); 
+                                            setSelectedValueProv(selectedProveedor);
                                         }} />
-                                        <ErrorMessage
+                                    <ErrorMessage
                                         name="proveedor"
                                         component={() => (<span><small className="text-danger p-2">{errors.proveedor}</small></span>)}
                                     />
@@ -200,7 +196,7 @@ export const AddRemito = () => {
                             <div className="row altaRemito">
                                 <div className="col">
                                     <label htmlFor="comentarios">COMENTARIOS</label>
-                                    <Field 
+                                    <Field
                                         id="comentarios"
                                         className="form-control"
                                         placeholder="El comentario no puede superar los  200 caracteres"
@@ -216,7 +212,11 @@ export const AddRemito = () => {
                             <div className="d-grid gap-2 d-md-flex justify-content-md-end">
                                 <Link to='/remitos'><button className="btn btn-primary" type="button">Volver</button> </Link>
                                 <button className='btn btn-primary' type="submit">Enviar</button>
-                                <button /* onClick={deleteCamposRemito} */ className="btn btn-primary" type="button">
+                                <button onClick={() => {
+                                    resetForm();
+                                    setSelectedValueProv({ label: 'Proveedores', value: '' });
+                                    setSelectedValueDep({ label: 'Departamentos', value: '' });
+                                }} className="btn btn-primary" type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash trashIcon" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
