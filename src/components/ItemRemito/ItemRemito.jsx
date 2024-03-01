0f1cd0027c9189ab5e179644829056b56dfeafee
Matias Aranda <sergiomatiasaranda@gmail.com>
2024-03-01 09:17:38 -0300

    Ajusto vista de los botones en el detalle del remito

--- src/components/ItemRemito/ItemRemito.jsx
+++ src/components/ItemRemito/ItemRemito.jsx
@@ -47,43 +47,7 @@ export const ItemRemito = () => {
         <>
             <div className="card cardRemito" >
                 <div className="card-body">
-                    <Nav className=" menuDescargasRemito" activeKey="/home">
-                        <Nav.Item>
-                            <NavLink to='/remitos' >
-                                <button className="btn btn-dark m-2">Volver</button>
-                            </NavLink>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <DownloadTableExcel
-                                filename='Remito_Perifericos_Tabla'
-                                sheet='Página 1'
-                                currentTableRef={tableRef.current}>
-                                <button className='btn btn-success m-2'>XLS</button>
-                            </DownloadTableExcel>
-                        </Nav.Item>
-                        {/* El preview de PDF está comentado porque me parece redundate previsualizar el PDF cuando se ve en pantalla
-                        y que haya otro botón para descargar cuando solo necesitamos un botón para descargar */}
-                        {/* <Nav.Item>
-                            <button onClick={() => {
-                                setVerPDF(!verPDF)
-                            }} className="btn btn-danger m-2">{!verPDF ? "PDF" : "cerrar PDF"}</button>
-                        </Nav.Item> */}
-                        <Nav.Item>
-                            <PDFDownloadLink document={<ItemRemitoPDF data={remitos} dataPerifericos={remitosPerifericos} name={name} />} fileName='Remito_Perifericos_PDF'>
-                                <button className='btn btn-danger m-2'> PDF {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
-                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-                                </svg> */}</button>
-                            </PDFDownloadLink>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <NavLink>
-                                <button className='btn btn-primary m-2' onClick={handleClicEntrega}>
-                                    Iniciar entrega
-                                </button>
-                            </NavLink>
-                        </Nav.Item>
-                    </Nav>
+                    
 
                     <h5 className="card-title">Remito N° {remitos.remito}</h5>
                     <>
@@ -186,7 +150,46 @@ export const ItemRemito = () => {
                         </>
                     </>
                 </div>
-            </div>
+                
+                <Nav className="card-footer d-flex justify-content-end" activeKey="/home">
+                        <Nav.Item>
+                            <NavLink to='/remitos' >
+                                <button className="btn btn-dark m-2">Volver</button>
+                            </NavLink>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <DownloadTableExcel
+                                filename='Remito_Perifericos_Tabla'
+                                sheet='Página 1'
+                                currentTableRef={tableRef.current}>
+                                <button className='btn btn-success m-2'>XLS</button>
+                            </DownloadTableExcel>
+                        </Nav.Item>
+                        {/* El preview de PDF está comentado porque me parece redundate previsualizar el PDF cuando se ve en pantalla
+                        y que haya otro botón para descargar cuando solo necesitamos un botón para descargar */}
+                        {/* <Nav.Item>
+                            <button onClick={() => {
+                                setVerPDF(!verPDF)
+                            }} className="btn btn-danger m-2">{!verPDF ? "PDF" : "cerrar PDF"}</button>
+                        </Nav.Item> */}
+                        <Nav.Item>
+                            <PDFDownloadLink document={<ItemRemitoPDF data={remitos} dataPerifericos={remitosPerifericos} name={name} />} fileName='Remito_Perifericos_PDF'>
+                                <button className='btn btn-danger m-2'> PDF {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                                </svg> */}</button>
+                            </PDFDownloadLink>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <NavLink>
+                                <button className='btn btn-primary m-2' onClick={handleClicEntrega}>
+                                    Iniciar entrega
+                                </button>
+                            </NavLink>
+                        </Nav.Item>
+                    </Nav>
+                </div>
+            
             <div>
                 <ModalEntrega data={remitos} dataPerifericos={remitosPerifericos} setShow={setShow} show={show}/>
             </div>
